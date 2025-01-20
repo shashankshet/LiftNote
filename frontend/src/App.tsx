9e311df1a1081fddd6b8498d87af9961ad708bd7
Shashank <48438739+shashankshet@users.noreply.github.com>
2025-01-20 20:23:59 +0530

    add: components shadcn/ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Example from "./components/Hero";
+import { HeroSection } from "./components/hero-section-dark";
+import { HeroSectionDemo } from "./components/hero-section-demo";
+import { HeroScrollDemo } from "./components/demo-scroll";
+
+export default function App() {
+  return (
+    <HeroScrollDemo/>
+  )
+}
---
+++ frontend/src/components/hero-section-demo.tsx
@@ -0,0 +1,29 @@
+import { HeroSection } from "./hero-section-dark"
+
+
+function HeroSectionDemo() {
+  return (
+    <HeroSection
+      title="Welcome to Our Platform"
+      subtitle={{
+        regular: "Transform your ideas into ",
+        gradient: "beautiful digital experiences",
+      }}
+      description="Transform your ideas into reality with our comprehensive suite of development tools and resources."
+      ctaText="Get Started"
+      ctaHref="/signup"
+      bottomImage={{
+        light: "https://www.launchuicomponents.com/app-light.png",
+        dark: "https://www.launchuicomponents.com/app-dark.png",
+      }}
+      gridOptions={{
+        angle: 65,
+        opacity: 0.4,
+        cellSize: 50,
+        lightLineColor: "#4a4a4a",
+        darkLineColor: "#2a2a2a",
+      }}
+    />
+  )
+}
+export { HeroSectionDemo }
