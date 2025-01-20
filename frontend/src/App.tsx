6f79530cff0269a2f98009e5bfa0a43b13006db5
Shashank <48438739+shashankshet@users.noreply.github.com>
2025-01-20 21:46:03 +0530

    build fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Hero } from "./components/hero";
+import { Hero } from "./components/Hero";
+
 
 
 export default function App() {
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 export function Hero() {
   const words = [
@@ -35,11 +34,14 @@ export function Hero() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+        {/* <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           Login
-        </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+        </button> */}
+        {/* <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
           Signup
+        </button> */}
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Comming soon
         </button>
       </div>
     </div>
