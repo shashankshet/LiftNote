e9eaf8b65fa12e9bbe12982c12451f6c2379d349
Shashank <48438739+shashankshet@users.noreply.github.com>
2025-01-20 22:54:11 +0530

    remove: login, signup button

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -40,12 +40,14 @@ export function Hero() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+        {/* commenting login and signup button */}
+
+        {/* <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           Login
         </button>
         <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
           Signup
-        </button>
+        </button> */}
         <button
           className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
           onClick={handleButtonClick}
